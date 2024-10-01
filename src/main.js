da2051d5a236fbf378d20be57eb3671c445f0e2e
AndyU18 <andres.urquidi@ucb.edu.bo>
2024-10-01 01:01:44 -0400

    Configuracion del menu, esperando a creacion de las vistas para cada opcion del menu

--- src/main.js
+++ src/main.js
@@ -3,9 +3,11 @@ import './style.css'
 import './index.css'
 import App from './App.vue'
 import router from './router'; 
+import Vuesax from 'vuesax3';
+import 'vuesax3/dist/vuesax.css'; // Importa el CSS de Vuesax
 
 const app = createApp(App);
 
 app.use(router); // Usa el router
-
+app.use(Vuesax); // Asegúrate de que Vuesax esté incluido aquí
 app.mount('#app');
--- src/views/MenuPage.vue
+++ src/views/MenuPage.vue
@@ -1,24 +1,89 @@
 <template>
-  <div>
-    <h1>Bienvenido al Menú</h1>
-    <p>Aquí puedes navegar a diferentes secciones.</p>
-    <button @click="goToInicio">Regresar a Inicio</button>
+  <div class="center">
+    <h1 class="digital-clock">GALAGEX</h1>
+
+    <vs-button class="menu-button" @click="handlePlay">
+      Jugar
+      <template #animate>
+        <i class='bx bx-game'></i>
+      </template>
+    </vs-button>
+
+    <vs-button
+      flat
+      success
+      class="menu-button"
+      animation-type="vertical"
+      @click="handleSettings"
+    >
+      Configuraciones
+      <template #animate>
+        <i class='bx bx-cog'></i>
+      </template>
+    </vs-button>
+
+    <vs-button
+      gradient
+      style="min-width: 60px"
+      warn
+      class="menu-button"
+      animation-type="scale"
+      @click="handleHowToPlay"
+    >
+      Cómo Jugar
+      <template #animate>
+        <i class='bx bx-help-circle'></i>
+      </template>
+    </vs-button>
+
+    <vs-button
+      icon
+      relief
+      danger
+      class="menu-button"
+      animation-type="rotate"
+      @click="handleExit"
+    >
+      Salir
+      <template #animate>
+        <i class='bx bx-exit'></i>
+      </template>
+    </vs-button>
   </div>
 </template>
 
 <script setup>
-// Importa useRouter desde vue-router
-import { useRouter } from 'vue-router';
+const handlePlay = () => {
+  // lógica para el botón Jugar
+};
 
-// Obtén el router
-const router = useRouter();
+const handleSettings = () => {
+  // lógica para el botón Configuraciones
+};
 
-// Define la función para navegar a la página de inicio
-const goToInicio = () => {
-  router.push('/'); // Usa la ruta correcta
+const handleHowToPlay = () => {
+  // lógica para el botón Cómo Jugar
+};
+
+const handleExit = () => {
+  // lógica para el botón Salir
 };
 </script>
 
 <style scoped>
-/* Estilos específicos para MenuPage */
+.center {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.digital-clock {
+  font-family: 'Digital', monospace; /* Cambia esto por la fuente que desees */
+  font-size: 70px;
+  margin-bottom: 35px;
+}
+
+.menu-button {
+  margin: 18px 0; /* Ajusta este valor para aumentar o disminuir el espaciado */
+}
 </style>
